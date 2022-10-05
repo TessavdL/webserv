7e61cf0c181f99426327ffbe3f33cdd6b68439ef
Jelle riense Van der kooi <jelvan-d@f0r6s2.codam.nl>
2022-10-05 14:59:33 +0200

    style: structured filestructure in accordance to best practice

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   parser.hpp                                         :+:    :+:            */
+/*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jelvan-d <jelvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/09/14 13:42:53 by jelvan-d      #+#    #+#                 */
-/*   Updated: 2022/09/14 14:43:03 by jelvan-d      ########   odam.nl         */
+/*   Created: 2022/10/05 14:38:25 by jelvan-d      #+#    #+#                 */
+/*   Updated: 2022/10/05 14:40:34 by jelvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	PARSER_HPP
-# define PARSER_HPP
+#include "../includes/parser/server_config.hpp"
+#include "../includes/*.hpp"
+#include "./*.hpp"
 
-# include <string>
-
-class Parser
-{
-	private:
-
-	protected:
-		
-	public:
-		Parser();
-		Parser(Parser const &other);
-		Parser &operator=(Parser const& rhs);
-		virtual~Parser();
-};
-
-#endif
