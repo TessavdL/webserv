0652d1a734bd7bafd784f6a709614893ba107cf9
Tessa Van der loo <tevan-de@f1r1s7.codam.nl>
2022-10-31 15:02:00 +0100

    feature: created a connectino class that keeps tracks of all the data that is required during the connection with the client

---
+++ includes/event_loop/connection.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   connection.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tevan-de <tevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/31 11:43:36 by tevan-de      #+#    #+#                 */
+/*   Updated: 2022/10/31 14:09:16 by tevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONNECTION_HPP
+# define CONNECTION_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "../../includes/config_parser/server.hpp"
+#include "../../includes/http_request_parser/http_request_lexer.hpp"
+#include "../../includes/http_response/response.hpp"
+
+class Connection {
+	public:
+		Connection();
+		~Connection();
+		Connection(Connection const& other);
+		Connection&	operator=(Connection const& other);
+		// int const&									get_connection_fd() const;
+		std::map<int, std::vector<Server> > const&	get_virtual_servers() const;
+		HTTPRequestLexer const&						get_request() const;
+		Response const&								get_response() const;
+		void										set_virtual_servers(std::pair<int, std::vector<Server> > virtual_servers);
+	private:
+		// int											_connection_fd;
+		std::pair<int, std::vector<Server> >		_virtual_servers;
+		HTTPRequestLexer							_request;
+		Response									_response;
+};
+
+#endif
---
+++ srcs/event_loop/connection.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   connection.cpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tevan-de <tevan-de@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/31 11:50:52 by tevan-de      #+#    #+#                 */
+/*   Updated: 2022/10/31 14:08:57 by tevan-de      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/event_loop/connection.hpp"
+
+Connection::Connection() {
+
+}
+
+Connection::~Connection() {
+
+}
+
+Connection::Connection(Connection const& other) {
+	*this = other;
+}
+
+Connection&	Connection::operator=(Connection const& other) {
+	if (this != &other) {
+		this->_connection_fd = other._connection_fd;
+		this->_virtual_servers = other._virtual_servers;
+		this->_request = other._request;
+		this->_response = other._response;
+	}
+	return (*this);
+}
+
+// int const&	Connection::get_connection_fd(void) const {
+// 	return (this->_connection_fd);
+// }
+
+std::map<int, std::vector<Server> > const&	Connection::get_virtual_servers(void) const {
+	return (this->_virtual_servers);
+}
+
+HTTPRequestLexer const&	Connection::get_request(void) const {
+	return (this->_request);
+}
+
+Response const&	Connection::get_response(void) const {
+	return (this->_response);
+}
+
+void	Connection::set_virtual_servers(std::pair<int, std::vector<Server> > virtual_servers) {
+	this->_virtual_servers = virtual_servers;
+}
