3cfc1b3145e298d31b581d10627f81111f8a8bc5
Tessa Van der loo <tevan-de@f1r3s10.codam.nl>
2022-11-14 16:40:08 +0100

    refactor: changed query map with key value pairs to a query string, cleanup: changed spaces to tabs

--- includes/http_request_parser/uri_parser.hpp
+++ includes/http_request_parser/uri_parser.hpp
@@ -5,8 +5,8 @@
 /*                                                     +:+                    */
 /*   By: tevan-de <tevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/11/02 16:37:21 by tevan-de      #+#    #+#                 */
-/*   Updated: 2022/11/02 16:48:43 by tevan-de      ########   odam.nl         */
+/*   Created: 2022/11/14 16:30:14 by tevan-de      #+#    #+#                 */
+/*   Updated: 2022/11/14 16:38:48 by tevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,35 @@
 # include "./utility_split.hpp"
 
 class Uri {
-    public:
-        Uri();
-        ~Uri();
-        Uri(Uri const& other);
-        Uri&                                        operator=(Uri const& other);
-        void                                        parse_uri(std::string const& str);
-        void                                        parse_scheme(std::string const& str, size_t& index);
-        void                                        parse_authority(std::string const& str, size_t& index);
-        void                                        parse_path(std::string const& str, size_t& index);
-        void                                        parse_query(std::string const& str, size_t index);
-        std::string const&                          get_scheme(void) const;
-        std::string const&                          get_authority_user_information(void) const;
-        std::string const&                          get_authority_host(void) const;
-        int const&                                  get_authority_port(void) const;
-        std::string const&                          get_path_full(void) const;
-        std::string const&                          get_path_extension(void) const;
-        std::string const&                          get_path_without_extension(void) const;
-        std::map<std::string, std::string> const&   get_query(void) const;        
-    private:
-        std::string							        _scheme;
-        std::string							        _authority_user_information;
-        std::string							        _authority_host;
-        int									        _authority_port;
-        std::string							        _path_full;
-        std::string							        _path_extension;
-        std::string							        _path_without_extension;
-        std::map<std::string, std::string>	        _query;
+	public:
+		Uri();
+		~Uri();
+		Uri(Uri const& other);
+		Uri&	operator=(Uri const& other);
+		void										parse_uri(std::string const& str);
+		void										parse_scheme(std::string const& str, size_t& index);
+		void										parse_authority(std::string const& str, size_t& index);
+		void										parse_path(std::string const& str, size_t& index);
+		void										parse_query(std::string const& str, size_t index);
+		std::string const&							get_scheme() const;
+		std::string const&							get_authority_user_information() const;
+		std::string const&							get_authority_host() const;
+		int const&									get_authority_port() const;
+		std::string const&							get_path_full() const;
+		std::string const&							get_path_extension() const;
+		std::string const&							get_path_without_extension() const;
+		// std::map<std::string, std::string> const&	get_query() const;
+		std::string const&							get_query_string() const;
+	private:
+		std::string									_scheme;
+		std::string									_authority_user_information;
+		std::string									_authority_host;
+		int											_authority_port;
+		std::string									_path_full;
+		std::string									_path_extension;
+		std::string									_path_without_extension;
+		// std::map<std::string, std::string>			_query;
+		std::string									_query_string;
 
 };
 
