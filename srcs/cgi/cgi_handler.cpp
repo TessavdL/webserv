4db8ddcdbd8652b1863739853bf46586a235e988
Tessa Van der loo <tevan-de@f0r2s5.codam.nl>
2023-01-07 21:10:59 +0100

    cleanup cgi_handler

--- srcs/cgi/cgi_handler.cpp
+++ srcs/cgi/cgi_handler.cpp
@@ -6,7 +6,7 @@
 /*   By: jelvan-d <jelvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/16 16:39:39 by jelvan-d      #+#    #+#                 */
-/*   Updated: 2023/01/03 17:23:36 by jelvan-d      ########   odam.nl         */
+/*   Updated: 2023/01/07 21:10:47 by tevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	create_cgi_response(Connection& connection, std::string const& file_locatio
 		status_line = "HTTP/1.1 200 OK\r\n";
 	else if (!connection.get_request().get_method().compare("POST"))
 		status_line = "HTTP/1.1 201 Created\r\n";
-	cout << "CGI BODY = " << cgi_session.get_body() << endl;
 	data.set_full_response(status_line + cgi_session.get_body());
 	connection.set_response(data);
 }
